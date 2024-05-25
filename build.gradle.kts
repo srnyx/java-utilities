9c1d795ac6d245bd9a5ad267a15a323de964601e
srnyx <25808801+srnyx@users.noreply.github.com>
2024-05-25 15:15:04 -0400

    Update Gradle plugins
    Gradle Galaxy: 1.1.2 -> 1.1.3
    Shadow: 8.1.1 -> 8.1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import xyz.srnyx.gradlegalaxy.utility.setupPublishing
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.1.2"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.srnyx.gradle-galaxy") version "1.1.3"
+    id("io.github.goooler.shadow") version "8.1.7"
 }
 
 repositories.mavenCentral()
