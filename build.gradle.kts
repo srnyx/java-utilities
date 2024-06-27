920e041b3bfcb80b66551e33187e61307f748c48
srnyx <25808801+srnyx@users.noreply.github.com>
2024-06-26 20:14:21 -0400

    Bump version (1.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,17 +6,18 @@ import xyz.srnyx.gradlegalaxy.utility.setupPublishing
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.1.3"
-    id("io.github.goooler.shadow") version "8.1.7"
+    id("xyz.srnyx.gradle-galaxy") version "1.2.2"
+    id("io.github.goooler.shadow") version "8.1.8"
 }
+
+setupJava("xyz.srnyx", "1.1.0", "General Java utility library for srnyx's projects", JavaVersion.VERSION_1_8)
 
 repositories.mavenCentral()
 dependencies {
-    compileOnly("org.jetbrains:annotations:24.0.1")
-    compileOnly("com.google.code.gson:gson:2.3.1")
+    compileOnly("org.jetbrains", "annotations", "24.1.0")
+    compileOnly("com.google.code.gson", "gson", "2.3.1") // Use this specific version for Spigot
 }
 
-setupJava("xyz.srnyx", "1.0.0", "General Java utility library for srnyx's projects", JavaVersion.VERSION_1_8)
 setupPublishing(
     artifactId = "java-utilities",
     url = "https://java-utilities.srnyx.com",
