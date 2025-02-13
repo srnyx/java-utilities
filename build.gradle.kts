5829fb7bb2b82c6db34109ad285f13a3d72f9032
srnyx <25808801+srnyx@users.noreply.github.com>
2025-02-13 18:02:37 -0500

    Update dependencies
    Gradle: 8.10.2 -> 8.12.1
    Gradle Galaxy: 1.3.2 -> 1.3.3
    Shadow: 8.3.3 -> 8.3.6
    annotations: 24.1.0 -> 26.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ import xyz.srnyx.gradlegalaxy.utility.setupPublishing
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.3.2"
-    id("com.gradleup.shadow") version "8.3.3"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.3"
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 setupJava("xyz.srnyx", "2.0.0", "General Java utility library for srnyx's projects", JavaVersion.VERSION_1_8)
 
 repositories.mavenCentral()
 dependencies {
-    compileOnly("org.jetbrains", "annotations", "24.1.0")
+    compileOnly("org.jetbrains", "annotations", "26.0.2")
     compileOnly("com.google.code.gson", "gson", "2.3.1") // Use this specific version for Spigot
 }
 
