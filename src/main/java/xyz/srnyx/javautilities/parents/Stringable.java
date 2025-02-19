2bfeda64855e81759ea00d6418bf004a9b92e0c6
srnyx <25808801+srnyx@users.noreply.github.com>
2025-02-19 16:43:50 -0500

    New `Stringable` methods and get superclass fields

--- src/main/java/xyz/srnyx/javautilities/parents/Stringable.java
+++ src/main/java/xyz/srnyx/javautilities/parents/Stringable.java
@@ -3,9 +3,10 @@ package xyz.srnyx.javautilities.parents;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
+import java.util.function.Predicate;
 
 
 /**
@@ -25,18 +26,27 @@ public class Stringable {
     }
 
     /**
-     * Converts an object to a {@link String} by getting all of its fields and their values
+     * Converts an object to a {@link String} by getting all of its fields and their values based on the given predicate
+     * <br>Will also get superclass fields
      *
-     * @param   object  the object to convert
+     * @param   object          the object to convert
+     * @param   fieldPredicate  the predicate to test the fields with (if null, all fields are included)
      *
-     * @return          the object as a {@link String}
+     * @return                  the object as a {@link String}
      */
     @NotNull
-    public static String toString(@Nullable Object object) {
+    public static String toString(@Nullable Object object, @Nullable Predicate<Field> fieldPredicate) {
         if (object == null) return "null";
         final Class<?> clazz = object.getClass();
-        return clazz.getSimpleName() + "{" + Arrays.stream(clazz.getDeclaredFields())
-                .filter(field -> !Modifier.isStatic(field.getModifiers()))
+
+        // Get declared fields and fields from all superclasses
+        final List<Field> fields = new ArrayList<>();
+        for (Class<?> c = clazz; c != null; c = c.getSuperclass()) fields.addAll(Arrays.asList(c.getDeclaredFields()));
+
+        // Create the string
+        final boolean fieldPredicateNull = fieldPredicate == null;
+        return clazz.getSimpleName() + "{" + fields.stream()
+                .filter(field -> !Modifier.isStatic(field.getModifiers()) && (fieldPredicateNull || fieldPredicate.test(field)))
                 .map(field -> {
                     final String entry;
                     try {
@@ -52,5 +62,45 @@ public class Stringable {
                 .filter(Objects::nonNull)
                 .reduce((a, b) -> a + ", " + b)
                 .orElse("") + "}";
+    }
+
+    /**
+     * Converts an object to a {@link String} by getting all of its fields and their values, excluding fields with the given classes
+     *
+     * @param   object          the object to convert
+     * @param   ignoredClasses  the classes to exclude from the conversion
+     *
+     * @return                  the object as a {@link String}
+     */
+    @NotNull
+    public static String toString(@Nullable Object object, @NotNull Class<?>... ignoredClasses) {
+        final Set<Class<?>> ignoredClassesSet = new HashSet<>(Arrays.asList(ignoredClasses));
+        return toString(object, field -> !ignoredClassesSet.contains(field.getType()));
+    }
+
+    /**
+     * Converts an object to a {@link String} by getting all of its fields and their values, excluding fields with the given names
+     *
+     * @param   object          the object to convert
+     * @param   ignoredFields   the names of the fields to exclude from the conversion
+     *
+     * @return                  the object as a {@link String}
+     */
+    @NotNull
+    public static String toString(@Nullable Object object, @NotNull String... ignoredFields) {
+        final Set<String> ignoredFieldsSet = new HashSet<>(Arrays.asList(ignoredFields));
+        return toString(object, field -> !ignoredFieldsSet.contains(field.getName()));
+    }
+
+    /**
+     * Converts an object to a {@link String} by getting all of its fields and their values
+     *
+     * @param   object  the object to convert
+     *
+     * @return          the object as a {@link String}
+     */
+    @NotNull
+    public static String toString(@Nullable Object object) {
+        return toString(object, (Predicate<Field>) null);
     }
 }
