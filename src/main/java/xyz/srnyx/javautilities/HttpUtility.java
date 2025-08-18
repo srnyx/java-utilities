d113a2f67b352e4f298e4c5b1033ac0574968b31
srnyx <25808801+srnyx@users.noreply.github.com>
2025-08-18 03:11:22 -0400

    Improve `HttpUtility`

--- src/main/java/xyz/srnyx/javautilities/HttpUtility.java
+++ src/main/java/xyz/srnyx/javautilities/HttpUtility.java
@@ -8,9 +8,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -21,6 +24,9 @@ import java.util.stream.Collectors;
  * Utility class for making HTTP requests
  */
 public class HttpUtility {
+    /**
+     * Debug mode (enabled stack traces and 404 messages logged to console)
+     */
     public static boolean DEBUG = false;
 
     /**
@@ -81,7 +87,39 @@ public class HttpUtility {
      */
     @NotNull
     public static Optional<JsonElement> getJson(@NotNull String userAgent, @NotNull String urlString, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
-        return get(userAgent, urlString, reader -> new JsonParser().parse(reader), connectionConsumer);
+        return get(userAgent, urlString, reader -> new JsonParser().parse(reader), connection -> {
+            connection.setRequestProperty("Accept", "application/json");
+            if (connectionConsumer != null) connectionConsumer.accept(connection);
+        });
+    }
+
+    /**
+     * Sends a POST request to the specified URL with the specified data
+     *
+     * @param   userAgent           the user agent to use
+     * @param   urlString           the URL to send the POST request to
+     * @param   data                the data to send with the POST request
+     * @param   connectionConsumer  the consumer to apply to the {@link HttpURLConnection}
+     *
+     * @return                      a {@link Response} object containing the response code and message, or null if the request failed
+     */
+    @NotNull
+    public static Optional<Response> post(@NotNull String userAgent, @NotNull String urlString, @Nullable byte[] data, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
+        Response response = null;
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) URI.create(urlString).toURL().openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("User-Agent", userAgent);
+            connection.setDoOutput(true);
+            if (connectionConsumer != null) connectionConsumer.accept(connection);
+            if (data != null) connection.getOutputStream().write(data);
+            response = new Response(connection.getResponseCode(), connection.getResponseMessage(), getResponseBody(connection));
+        } catch (final Exception e) {
+            if (DEBUG) e.printStackTrace();
+        }
+        if (connection != null) connection.disconnect();
+        return Optional.ofNullable(response);
     }
 
     /**
@@ -94,24 +132,41 @@ public class HttpUtility {
      *
      * @return                      a {@link Response} object containing the response code and message, or null if the request failed
      */
-    @Nullable
-    public static Response postJson(@NotNull String userAgent, @NotNull String urlString, @Nullable JsonElement data, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
-        Response response = null;
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) URI.create(urlString).toURL().openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("User-Agent", userAgent);
+    @NotNull
+    public static Optional<Response> postJson(@NotNull String userAgent, @NotNull String urlString, @Nullable JsonElement data, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
+        return post(userAgent, urlString, data != null ? data.toString().getBytes() : null, connection -> {
             connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
             if (connectionConsumer != null) connectionConsumer.accept(connection);
-            if (data != null) connection.getOutputStream().write(data.toString().getBytes());
-            response = new Response(connection.getResponseCode(), connection.getResponseMessage());
-        } catch (final Exception e) {
-            if (DEBUG) e.printStackTrace();
-        }
-        if (connection != null) connection.disconnect();
-        return response;
+        });
+    }
+
+    /**
+     * Sends a POST request to the specified URL with the specified form data
+     *
+     * @param   userAgent           the user agent to use
+     * @param   urlString           the URL to send the POST request to
+     * @param   formData            the form data to send with the POST request
+     * @param   connectionConsumer  the consumer to apply to the {@link HttpURLConnection}
+     *
+     * @return                      a {@link Response} object containing the response code and message, or null if the request failed
+     */
+    @NotNull
+    public static Optional<Response> postFormUrlEncoded(@NotNull String userAgent, @NotNull String urlString, @NotNull Map<String, String> formData, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
+        final String formBody = formData.entrySet().stream()
+                .map(entry -> {
+                    try {
+                        return URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.name()) + "=" + URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.name());
+                    } catch (final Exception e) {
+                        if (DEBUG) e.printStackTrace();
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining("&"));
+        return post(userAgent, urlString, formBody.getBytes(), connection -> {
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            if (connectionConsumer != null) connectionConsumer.accept(connection);
+        });
     }
 
     /**
@@ -124,8 +179,8 @@ public class HttpUtility {
      *
      * @return                      a {@link Response} object containing the response code and message, or null if the request failed
      */
-    @Nullable
-    public static Response putJson(@NotNull String userAgent, @NotNull String urlString, @Nullable JsonElement data, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
+    @NotNull
+    public static Optional<Response> putJson(@NotNull String userAgent, @NotNull String urlString, @Nullable JsonElement data, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
         Response response = null;
         HttpURLConnection connection = null;
         try {
@@ -136,12 +191,12 @@ public class HttpUtility {
             connection.setDoOutput(true);
             if (connectionConsumer != null) connectionConsumer.accept(connection);
             if (data != null) connection.getOutputStream().write(data.toString().getBytes());
-            response = new Response(connection.getResponseCode(), connection.getResponseMessage());
+            response = new Response(connection.getResponseCode(), connection.getResponseMessage(), getResponseBody(connection));
         } catch (final Exception e) {
             if (DEBUG) e.printStackTrace();
         }
         if (connection != null) connection.disconnect();
-        return response;
+        return Optional.ofNullable(response);
     }
 
     /**
@@ -153,8 +208,8 @@ public class HttpUtility {
      *
      * @return                      a {@link Response} object containing the response code and message, or null if the request failed
      */
-    @Nullable
-    public static Response delete(@NotNull String userAgent, @NotNull String urlString, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
+    @NotNull
+    public static Optional<Response> delete(@NotNull String userAgent, @NotNull String urlString, @Nullable Consumer<HttpURLConnection> connectionConsumer) {
         Response response = null;
         HttpURLConnection connection = null;
         try {
@@ -162,12 +217,31 @@ public class HttpUtility {
             connection.setRequestMethod("DELETE");
             connection.setRequestProperty("User-Agent", userAgent);
             if (connectionConsumer != null) connectionConsumer.accept(connection);
-            response = new Response(connection.getResponseCode(), connection.getResponseMessage());
+            response = new Response(connection.getResponseCode(), connection.getResponseMessage(), getResponseBody(connection));
         } catch (final Exception e) {
             if (DEBUG) e.printStackTrace();
         }
         if (connection != null) connection.disconnect();
-        return response;
+        return Optional.ofNullable(response);
+    }
+
+    /**
+     * Gets the response body from the specified {@link HttpURLConnection}
+     *
+     * @param   connection  the {@link HttpURLConnection} to get the response body from
+     *
+     * @return              the response body, or null if an error occurred
+     */
+    @Nullable
+    private static String getResponseBody(@NotNull HttpURLConnection connection) {
+        try (
+                final InputStream inputStream = connection.getInputStream();
+                final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        } catch (final Exception e) {
+            if (DEBUG) e.printStackTrace();
+        }
+        return null;
     }
 
     /**
@@ -191,16 +265,22 @@ public class HttpUtility {
          * The HTTP response message
          */
         @Nullable public final String message;
+        /**
+         * The HTTP response body
+         */
+        @Nullable public final String body;
 
         /**
          * Constructs a new {@link Response} instance
          *
          * @param   code    {@link #code}
          * @param   message {@link #message}
+         * @param   body    {@link #body}
          */
-        public Response(int code, @Nullable String message) {
+        public Response(int code, @Nullable String message, @Nullable String body) {
             this.code = code;
             this.message = message;
+            this.body = body;
         }
     }
 }
